67159119e557f053228a42b2297cf9228a6f0b7d
Fahad <fsadakathussain@gmail.com>
2025-09-06 14:48:43 +0600

    Add Dagger Hilt modules for dependency injection of user preferences, network monitoring, database, and repositories

---
+++ app/src/main/java/dev/sadakat/technonexttest/di/NetworkModule.kt
@@ -0,0 +1,62 @@
+package dev.sadakat.technonexttest.di
+
+import android.content.Context
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.sadakat.technonexttest.data.local.JsonDataSource
+import dev.sadakat.technonexttest.data.remote.api.PostsApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL).client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePostsApiService(retrofit: Retrofit): PostsApiService {
+        return retrofit.create(PostsApiService::class.java)
+    }
+
+
+    @Provides
+    @Singleton
+    fun providesGson(): Gson {
+        return Gson()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideJsonDataSource(@ApplicationContext context: Context, gson: Gson): JsonDataSource {
+        return JsonDataSource(context, gson)
+    }
+
+
+}
